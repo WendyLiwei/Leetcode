d565db0658542d990d1bc8475b00a5c70da5c067
Wendy Hsieh <wendyhsieh@WendydeMacBook-Pro.local>
2025-05-08 23:49:12 +0800

    191. Number of 1 Bits

---
+++ Blind75/191.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+        int hammingWeight(int n) {
+            int output=0;
+            for(int i=0;i<32;i++){
+                if((n>>i)&1==1)
+                    output=output+1;
+            }
+            return output;
+        }
+    };
