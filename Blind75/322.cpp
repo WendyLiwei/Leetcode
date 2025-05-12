d973f04d0b7ac9b6c0ca4b6fda293e44930a2b93
Wendy Hsieh <wendyhsieh@WendydeMacBook-Pro.local>
2025-05-12 23:37:38 +0800

    322. Coin Change

---
+++ Blind75/322.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int coinChange(vector<int>& coins, int amount) {
+            vector<int> dp(amount+1, INT_MAX);
+            dp[0]=0;
+            for(int i=0;i<coins.size();i++){
+                int coin=coins[i];
+                for(int j=coin;j<=amount;j++){
+                    if(dp[j-coin]!=INT_MAX){
+                        dp[j]=min(dp[j],dp[j-coin]+1);
+                    }
+                }
+            }
+            if(dp[amount]==INT_MAX)
+                return -1;
+            return dp[amount];
+        }
+    };
